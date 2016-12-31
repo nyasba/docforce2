9b120b4de447769146a273fd0826718762db51e1
nyasba <nyasba@gmail.com>
2016-12-31 22:39:48 +0900

    CellUtilのjavadocを追記

--- src/main/groovy/jp/nyasba/tool/docforce2/repository/CellUtil.groovy
+++ src/main/groovy/jp/nyasba/tool/docforce2/repository/CellUtil.groovy
@@ -9,12 +9,32 @@ import org.apache.poi.ss.usermodel.Sheet
  */
 class CellUtil {
     
+    /**
+     * セルを作成し、値をセットする
+     *
+     * @param sheet シート
+     * @param rowNumber 行番号
+     * @param cellNumber 列番号
+     * @param value 値
+     * @param style スタイル
+     * @return なし
+     */
     def static setValue(Sheet sheet, int rowNumber, int cellNumber, def value, CellStyle style) {
         Cell cell = sheet.getRow(rowNumber).createCell(cellNumber)
         cell.setCellValue(value)
         cell.setCellStyle(style)
     }
     
+    /**
+     * 行およびセルを作成し、値をセットする
+     *
+     * @param sheet シート
+     * @param rowNumber 行番号
+     * @param cellNumber 列番号
+     * @param value 値
+     * @param style スタイル
+     * @return なし
+     */
     def static setValueWithCreateRecord(Sheet sheet, int rowNumber, int cellNumber, def value, CellStyle style) {
         sheet.createRow(rowNumber)
         setValue(sheet, rowNumber, cellNumber, value, style)
