605f8586b53a7fd73427d3602fce338ec1343f55
nyasba <nyasba@gmail.com>
2017-01-04 12:55:56 +0900

    設定ファイルをyaml化

--- src/main/groovy/jp/nyasba/tool/docforce2/controller/SfdcGenerateExcelController.groovy
+++ src/main/groovy/jp/nyasba/tool/docforce2/controller/SfdcGenerateExcelController.groovy
@@ -1,10 +1,10 @@
 package jp.nyasba.tool.docforce2.controller
 
-import groovy.json.JsonSlurper
 import jp.nyasba.tool.docforce2.domain.SfdcCustomObject
 import jp.nyasba.tool.docforce2.domain.approvalprocess.SfdcApprovalProcess
 import jp.nyasba.tool.docforce2.domain.workflow.SfdcWorkflow
 import jp.nyasba.tool.docforce2.repository.ObjectDesignExcelBookRepository
+import org.yaml.snakeyaml.Yaml
 
 import java.nio.file.Paths
 
@@ -15,17 +15,28 @@ class SfdcGenerateExcelController {
     
     static void main(String[] args){
 
-        def jsonPath = Paths.get("C:/dev/sfdc/docforce2/input/setting.json")
-        def json = new JsonSlurper().parseText(jsonPath.text)
+//        def jsonPath = Paths.get("C:/dev/sfdc/docforce2/input/setting.json")
+//        def json = new JsonSlurper().parseText(jsonPath.text)
+//        // サンプル
+//        String inputBaseDir = json.inputBaseDir
+//        if(!inputBaseDir.endsWith("/")){
+//            inputBaseDir = inputBaseDir + "/"
+//        }
+//        String inputObjectPath = inputBaseDir + json.resource.object
+//        String inputApprovalProcessPath1 = inputBaseDir + json.resource.approvalProcesses.get(0)
+//        String inputWorkflowPath = inputBaseDir + json.resource.workflow
+        
+        def yamlPath = Paths.get("C:/dev/sfdc/docforce2/input/setting.yml")
+        def yaml = new Yaml().load(yamlPath.text)
 
-        // サンプル
-        String inputBaseDir = json.inputBaseDir
+        String inputBaseDir = yaml.inputBaseDir
         if(!inputBaseDir.endsWith("/")){
             inputBaseDir = inputBaseDir + "/"
         }
-        String inputObjectPath = inputBaseDir + json.resource.object
-        String inputApprovalProcessPath1 = inputBaseDir + json.resource.approvalProcesses.get(0)
-        String inputWorkflowPath = inputBaseDir + json.resource.workflow
+        String inputObjectPath = inputBaseDir + yaml.resource.object
+        String inputApprovalProcessPath1 = inputBaseDir + yaml.resource.approvalProcesses.get(0)
+        String inputWorkflowPath = inputBaseDir + yaml.resource.workflow
+
         
         SfdcCustomObject object = createObject(inputObjectPath)
         SfdcApprovalProcess approvalProcess1 = createApprovalProcess(inputApprovalProcessPath1)
