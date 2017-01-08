008098498f51ae958c9d947a468a60e75a6cfbfe
nyasba <nyasba@gmail.com>
2017-01-08 14:36:26 +0900

    複数リソースを設定可能にした

--- src/main/groovy/jp/nyasba/tool/docforce2/controller/SfdcGenerateExcelController.groovy
+++ src/main/groovy/jp/nyasba/tool/docforce2/controller/SfdcGenerateExcelController.groovy
@@ -18,37 +18,57 @@ class SfdcGenerateExcelController {
         def yamlPath = Paths.get("C:/dev/sfdc/docforce2/input/setting.yml")
         def yaml = new Yaml().load(yamlPath.text)
 
+        String inputBaseDir = getInputBaseDir(yaml)
+        String outputDir = getOutputDir(yaml)
+    
+        yaml.resources.collect{ createResouceDocument(inputBaseDir, outputDir, it) }
+    }
+    
+    private static String getInputBaseDir(def yaml){
         String inputBaseDir = yaml.inputBaseDir
-        String outputDir = yaml.outputDir
         if(!inputBaseDir.endsWith("/")){
             inputBaseDir = inputBaseDir + "/"
         }
-        String inputObjectPath = inputBaseDir + yaml.resource.object
-        String inputApprovalProcessPath1 = inputBaseDir + yaml.resource.approvalProcesses.get(0)
-        String inputWorkflowPath = inputBaseDir + yaml.resource.workflow
-
-        
-        SfdcCustomObject object = createObject(inputObjectPath)
-        SfdcApprovalProcess approvalProcess1 = createApprovalProcess(inputApprovalProcessPath1)
-        SfdcWorkflow workflow = createWorkflow(inputWorkflowPath)
-        
-        new ObjectDesignExcelBookRepository(outputDir).save(object, [approvalProcess1], workflow)
+        return inputBaseDir
     }
     
-    private static SfdcCustomObject createObject(String inputPath){
-        def xmlFile = Paths.get(inputPath)
-        def fileName = inputPath.find('[^/]*$')
+    private static String getOutputDir(def yaml){
+        return yaml.outputDir
+    }
+    
+    private static void createResouceDocument(String inputBaseDir, String outputDir, def resourceYaml){
+        SfdcCustomObject object = createObject(inputBaseDir, resourceYaml.resource?.object)
+        List<SfdcApprovalProcess> approvalProcessList = createApprovalProcessList(inputBaseDir, resourceYaml.resource?.approvalProcesses)
+        SfdcWorkflow workflow = createWorkflow(inputBaseDir, resourceYaml.resource?.workflow)
+    
+        new ObjectDesignExcelBookRepository(outputDir).save(object, approvalProcessList, workflow)
+    }
+    
+    private static SfdcCustomObject createObject(String inputBaseDir, String param){
+        assert param != null
+        String inputObjectPath = inputBaseDir + param
+        def xmlFile = Paths.get(inputObjectPath)
+        def fileName = inputObjectPath.find('[^/]*$')
         return new SfdcCustomObject(fileName, xmlFile.text)
     }
     
-    private static SfdcApprovalProcess createApprovalProcess(String inputPath){
-        def xmlFile = Paths.get(inputPath)
-        def fileName = inputPath.find('/[^/]*$')
-        return new SfdcApprovalProcess(fileName, xmlFile.text)
+    private static List<SfdcApprovalProcess> createApprovalProcessList(String inputBaseDir, def paramList){
+        if(paramList == null || !(paramList instanceof List)){
+            return []
+        }
+        return paramList.collect{
+            String inputApprovalProcessPath = inputBaseDir + it
+            def xmlFile = Paths.get(inputApprovalProcessPath)
+            def fileName = inputApprovalProcessPath.find('[^/]*$')
+            return new SfdcApprovalProcess(fileName, xmlFile.text)
+        }
     }
     
-    private static SfdcWorkflow createWorkflow(String inputPath){
-        def xmlFile = Paths.get(inputPath)
+    private static SfdcWorkflow createWorkflow(String inputBaseDir, String param){
+        assert param != null
+        String inputWorkflowPath = inputBaseDir + param
+        def xmlFile = Paths.get(inputWorkflowPath)
+        def fileName = inputWorkflowPath.find('[^/]*$')
         return new SfdcWorkflow(xmlFile.text)
     }
     
